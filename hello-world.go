4c1b96a70096705ee9b0f851c387e84d0926def2
k0t3n <k0t3n.mail@gmail.com>
2018-06-18 22:17:35 +0300

    Add hello-world and types-and-var

---
+++ hello-world.go
@@ -0,0 +1,10 @@
+// simple hello-world code
+
+package main
+
+import "fmt"
+
+func main() {
+  var helloWorld string = "Hello world!"
+  fmt.Println(helloWorld)
+}
---
+++ types-and-var.go
@@ -0,0 +1,15 @@
+// types and variables
+
+package main
+
+import "fmt"
+
+func main() {
+  var simpleString string = "Hello, I'm string variable!"
+  var simpleInt int = 1
+  var simpleFloat float64 = 1488.228
+  var simpleBool bool = true
+  superVar := "I'm lifehacked var!"
+
+  fmt.Println(simpleString, simpleInt, simpleFloat, simpleBool, superVar)
+}
